dc9d9f9dfd233426a69b9b5d0d49bdceb46676f2
Philip Withnall <pwithnall@gnome.org>
2024-04-19 18:46:17 +0100

    user: Split out helper function to unregister extensions
    This makes no functional difference, but will make an upcoming commit a
    little tidier.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    
    Helps: #125

--- src/user.c
+++ src/user.c
@@ -966,6 +966,23 @@ user_register_extensions (User *user)
         }
 }
 
+static void
+user_unregister_extensions (User *user)
+{
+        if (user->extension_ids != NULL) {
+                for (guint i = 0; i < user->n_extension_ids; i++) {
+                        /* In theory, if an error happened during registration, we could have 0 here. */
+                        if (user->extension_ids[i] == 0)
+                                continue;
+
+                        g_dbus_connection_unregister_object (user->system_bus_connection, user->extension_ids[i]);
+                }
+
+                g_clear_pointer (&user->extension_ids, g_free);
+                user->n_extension_ids = 0;
+        }
+}
+
 static gchar *
 compute_object_path (User *user)
 {
@@ -1037,20 +1054,7 @@ user_unregister (User *user)
 
         g_dbus_interface_skeleton_unexport (G_DBUS_INTERFACE_SKELETON (user));
 
-        if (user->extension_ids) {
-                guint i;
-
-                for (i = 0; i < user->n_extension_ids; i++) {
-                        /* In theory, if an error happened during registration, we could have 0 here. */
-                        if (user->extension_ids[i] == 0)
-                                continue;
-
-                        g_dbus_connection_unregister_object (user->system_bus_connection, user->extension_ids[i]);
-                }
-
-                g_clear_pointer (&user->extension_ids, g_free);
-                user->n_extension_ids = 0;
-        }
+        user_unregister_extensions (user);
 }
 
 void
